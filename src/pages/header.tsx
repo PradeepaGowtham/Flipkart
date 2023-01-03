3fabeaad07a8e4a939cb5c7309e527226388776b
PradeepaGowtham <pradeepa7895@gmail.com>
2023-01-03 15:03:28 +0530

    header function added

--- src/pages/header.tsx
+++ src/pages/header.tsx
@@ -1,12 +1,23 @@
-<div className="container">
-<div className="header">
-    <div className="flipkart">Flipkart</div>
-    <div className="grocery">Grocery</div>
-</div>
-<div className="priceDetails">PRICE DETAILS</div>
+import React from "react";
+import './header.css';
 
-<div className="address">
-   <p className="first">Deliver to: Suseendiran P,637211 <button className="work">WORK</button></p>
-   <p className="second">Lakshmi complex,North Car Street,Tiruchengode  <button className="change">Change</button></p>
-</div>
-</div>
+
+const Header = () => {
+return(
+    <>
+        <div className="container">
+        <div className="header">
+             <div className="flipkart">Flipkart</div>
+             <div className="grocery">Grocery</div>
+        </div>
+             <div className="priceDetails">PRICE DETAILS</div>
+
+        <div className="address">
+            <p className="first">Deliver to: Suseendiran P,637211 <button className="work">WORK</button></p>
+            <p className="second">Lakshmi complex,North Car Street,Tiruchengode  <button className="change">Change</button></p>
+        </div>
+        </div>
+    </>
+)
+}
+export default Header;
