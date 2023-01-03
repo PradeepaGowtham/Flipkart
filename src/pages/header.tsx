ad5365c6608a1859efaa180b8bac385e67000af7
PradeepaGowtham <pradeepa7895@gmail.com>
2023-01-03 14:52:52 +0530

    header file added

---
+++ src/pages/header.tsx
@@ -0,0 +1,12 @@
+<div className="container">
+<div className="header">
+    <div className="flipkart">Flipkart</div>
+    <div className="grocery">Grocery</div>
+</div>
+<div className="priceDetails">PRICE DETAILS</div>
+
+<div className="address">
+   <p className="first">Deliver to: Suseendiran P,637211 <button className="work">WORK</button></p>
+   <p className="second">Lakshmi complex,North Car Street,Tiruchengode  <button className="change">Change</button></p>
+</div>
+</div>
