45256f2325f806a6fb9b2533c06bcefc9e16c20f
PradeepaGowtham <pradeepa7895@gmail.com>
2023-02-07 11:57:02 +0530

    packages installed

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,18 +3,23 @@ import Navbar from './pages/navbar';
 import Header from './pages/header';
 import Button from './pages/button';
 import ProductsList from './components/mainpage/productslist';
+import { Route, Routes } from "react-router-dom";
 
+import AddProduct from './components/mainpage/addproduct';
 
 const App = () => {
   return (
     <React.Fragment>
-    <>
+  
       <Navbar />
       <Header />
       <ProductsList/>
       <Button />
-    </>
+    <Routes>
+      <Route path="/addproduct" element={<AddProduct />} />
+    </Routes>
     </React.Fragment>
+   
   )
 }
 
